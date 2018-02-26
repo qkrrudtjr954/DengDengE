47cd5d939b28882e22fa927f1629851e69d67e40
Mj <hello@naver.com>
2018-02-26 15:20:26 +0900

    대충 리스트 완성

--- src/controller/CommuBbsController.java
+++ src/controller/CommuBbsController.java
@@ -103,7 +103,7 @@ public class CommuBbsController extends HttpServlet {
 			req.setAttribute("bbslist", bbslist);
 			
 			//보내주기
-			dispatch("CommuBbslist.jsp", req, resp);
+			dispatch("TestList.jsp", req, resp);
 			
 		}else if(command.equals("update")) {
 			String Sseq = req.getParameter("seq");
