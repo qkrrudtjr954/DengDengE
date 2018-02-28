db489d4779c1f0e9739b26239971a85cf47f6356
HyenSun <dkfkehs02@naver.com>
2018-02-28 12:55:31 +0900

    검색결과 제한 완료

--- src/controller/AfterBbsController.java
+++ src/controller/AfterBbsController.java
@@ -27,199 +27,179 @@ public class AfterBbsController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doProcess(req, resp);
 	}
-	
+
 	public void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html; charset=UTF-8");
 		AfterBbsService bbs = AfterBbsService.getInstance();
 		AfterBbsDto dto = new AfterBbsDto();
 		String command = req.getParameter("command");
-		
-		
-		
-		if(command.equals("AfterBbslist")) {
-			
+
+		if (command.equals("AfterBbslist")) {
+
 			List<AfterBbsDto> afterBbslist = bbs.getAfterlBbsList();
 			req.setAttribute("afterBbslist", afterBbslist);
-			//bbs.readCount(seq);
-			
-			dispatch("AfterBbslist.jsp", req, resp);
-		//글쓰기	
-		}else if(command.equals("AfterBbswrite")) {
+			// bbs.readCount(seq);
 
-			if(Delegator.checkSession(req, resp)) {
-				// 로그인이 되어있는 상태 
-				dispatch("AfterBbswrite.jsp", req, resp);				
+			dispatch("AfterBbslist.jsp", req, resp);
+			// 글쓰기
+		} else if (command.equals("AfterBbswrite")) {
+
+			if (Delegator.checkSession(req, resp)) {
+				// 로그인이 되어있는 상태
+				dispatch("AfterBbswrite.jsp", req, resp);
 			} else {
-				// 로그인이 안된 상태 
+				// 로그인이 안된 상태
 				req.setAttribute("returnurl", "AfterBbsController?command=AfterBbswrite");
 				dispatch("UserControl?command=goSignIn", req, resp);
 			}
-			
-		// 글쓰기 처리	
-		}else if(command.equals("AfterBbswriteAf")) {
+
+			// 글쓰기 처리
+		} else if (command.equals("AfterBbswriteAf")) {
 			String title = req.getParameter("title");
 			String content = req.getParameter("content");
-			//String pic1 = req.getParameter("pic1");
-			//String suserseq = req.getParameter("userseq");
-			//int userseq = Integer.parseInt(suserseq);
-			
-			System.out.println(title+" " + content + " ");
-			
-			/*if(isNull(id) && isNull(title) && isNull(content)) {
-				req.setAttribute("msg1", "글쓰기 실패");
-				dispatch("bbswrite.jsp", req, resp);
-			}*/
-			
+			// String pic1 = req.getParameter("pic1");
+			// String suserseq = req.getParameter("userseq");
+			// int userseq = Integer.parseInt(suserseq);
+
+			System.out.println(title + " " + content + " ");
+
+			/*
+			 * if(isNull(id) && isNull(title) && isNull(content)) { req.setAttribute("msg1",
+			 * "글쓰기 실패"); dispatch("bbswrite.jsp", req, resp); }
+			 */
+
 			HttpSession session = req.getSession();
-            User userInfo = (User)session.getAttribute("current_user");
-            String writer = userInfo.getEmail();
-            int target_user_seq =userInfo.getSeq();
-			
-			
+			User userInfo = (User) session.getAttribute("current_user");
+			String writer = userInfo.getEmail();
+			int target_user_seq = userInfo.getSeq();
+
 			boolean isS = bbs.wirtelAfterBbs(new AfterBbsDto(title, content, target_user_seq));
 			Cookie cookie = null;
-			if(isS) {
+			if (isS) {
 				System.out.println(isS);
-				/*req.setAttribute("msg1", "글작성 완료");
-				dispatch("AfterBbsController?command=AfterBbslist", req, resp);*/
-				
-				
-				/*cookie = new Cookie("afterwirtemsg", "글작성완료");
-				cookie.setMaxAge(5);
-				resp.addCookie(cookie);*/
-								
+				/*
+				 * req.setAttribute("msg1", "글작성 완료");
+				 * dispatch("AfterBbsController?command=AfterBbslist", req, resp);
+				 */
+
+				/*
+				 * cookie = new Cookie("afterwirtemsg", "글작성완료"); cookie.setMaxAge(5);
+				 * resp.addCookie(cookie);
+				 */
+
 				resp.sendRedirect("AfterBbsController?command=AfterBbslist");
-				
-			}else {
+
+			} else {
 				System.out.println(isS);
 				req.setAttribute("mag1", "글작성 실패");
 				dispatch("AfterBbswrite.jsp", req, resp);
 			}
-			//디테일
-		}else if(command.equals("AfterBbsDetail")) {
-			
-			//dispatch("AfterBbsDetail.jsp", req, resp);
-			
-			String sseq = req.getParameter("seq"); 
-			System.out.println("detail-sseq: "+sseq);
-			
+			// 디테일
+		} else if (command.equals("AfterBbsDetail")) {
+
+			// dispatch("AfterBbsDetail.jsp", req, resp);
+
+			String sseq = req.getParameter("seq");
+			System.out.println("detail-sseq: " + sseq);
+
 			int seq = Integer.parseInt(sseq);
 			System.out.println("seq + " + seq);
-			
-			if(Delegator.checkSession(req, resp)) {
+
+			if (Delegator.checkSession(req, resp)) {
 				bbs.readCount(seq);
-				
+
 				AfterBbsDto bbs1 = bbs.detailAfterlBbs(seq);
-				System.out.println("Combbs1 = "  + bbs1);
+				System.out.println("Combbs1 = " + bbs1);
 				req.setAttribute("bbs1", bbs1);
-				// 로그인이 되어있는 상태 
-				//dispatch("AfterBbsDetail.jsp", req, resp);
-				dispatch("AfterBbsDetail.jsp" , req , resp);
+				// 로그인이 되어있는 상태
+				// dispatch("AfterBbsDetail.jsp", req, resp);
+				dispatch("AfterBbsDetail.jsp", req, resp);
 			} else {
-				// 로그인이 안된 상태 
+				// 로그인이 안된 상태
 				req.setAttribute("returnurl", "AfterBbsController?command=AfterBbsDetail&seq=" + seq);
 				dispatch("UserControl?command=goSignIn", req, resp);
 			}
-			
-			
-		}else if(command.equals("AfterBbsUpdate")) {  
+
+		} else if (command.equals("AfterBbsUpdate")) {
 			String sseq = req.getParameter("seq");
-			System.out.println("Update-sseq: "+sseq);
+			System.out.println("Update-sseq: " + sseq);
 			int seq = Integer.parseInt(sseq);
 			AfterBbsDto bbs2 = bbs.getBbs(seq);
-			//req.setAttribute("seq", seq);
+			// req.setAttribute("seq", seq);
 			req.setAttribute("bbs2", bbs2);
 			dispatch("AfterBbsUpdate.jsp", req, resp);
-			
-		}else if(command.equals("AfterBbsUpdateAf")){
+
+		} else if (command.equals("AfterBbsUpdateAf")) {
 			String seq = req.getParameter("seq");
 			String title = req.getParameter("title");
 			String content = req.getParameter("content");
-			//String pic1 = req.getParameter("pic1");
-			//String suserseq = req.getParameter("userseq");
-			//int userseq = Integer.parseInt(suserseq);
-			
+			// String pic1 = req.getParameter("pic1");
+			// String suserseq = req.getParameter("userseq");
+			// int userseq = Integer.parseInt(suserseq);
+
 			dto.setSeq(Integer.parseInt(seq));
 			dto.setTitle(title);
 			dto.setContent(content);
-			
-			System.out.println("updateBbsToString>>"+bbs.toString());
-			
+
+			System.out.println("updateBbsToString>>" + bbs.toString());
+
 			boolean isS = bbs.AfrerBbsUpdate(dto);
 			req.setAttribute("isS", isS);
-			
-			
-			if(isS) {
-				//JOptionPane.showMessageDialog(null, "수정성공");
+
+			if (isS) {
+				// JOptionPane.showMessageDialog(null, "수정성공");
 				req.setAttribute("msg", "수정성공");
 				dispatch("AfterBbsController?command=AfterBbslist", req, resp);
-				
-			}else {
-				//JOptionPane.showMessageDialog(null, "수정실패");
+
+			} else {
+				// JOptionPane.showMessageDialog(null, "수정실패");
 				req.setAttribute("msg", "수정실패");
 				dispatch("AfterBbsController?command=AfterBbsDetail", req, resp);
-				
-			}	
-			
-			
-		}else if(command.equals("AfterDelete")) {
-			
+
+			}
+
+		} else if (command.equals("AfterDelete")) {
+
 			String Sseq = req.getParameter("seq");
-	         int seq = Integer.parseInt(Sseq);
-	         System.out.println("삭제 시퀀스 : " + Sseq);
-	         
-	         
-	         
-	         boolean isS = bbs.AfterdeletBbs(seq);
-	         if(isS) {
-	            req.setAttribute("msg2", "삭제했습니다");
-	            dispatch("AfterBbsController?command=AfterBbslist", req, resp);
-	         }else { 
-	            req.setAttribute("msg2", "삭제 실패");
-	            dispatch("AfterBbsController?command=AfterBbsDetail&seq="+seq, req, resp);
-	         }
-			
-			
-		}else if(command.equals("AfterBbsSearch")) {
-	         
-	           String Searchtype = req.getParameter("Searchtype");             //검색종류(글쓴이,제목,내용)
-	           String SearchWord = req.getParameter("SearchWord");   
-	           
-	           
-	           System.out.println(" search " + Searchtype +" word "+SearchWord);
-	          
-	           List<AfterBbsDto> afterBbslist = bbs.getFindAfterlist(Searchtype, SearchWord);
-	            //짐 싸기
-	            req.setAttribute("afterBbslist", afterBbslist);
-	            
-	            //보내주기
-	            dispatch("AfterBbslist.jsp", req, resp);
-	           
-	      }
-		
-		
+			int seq = Integer.parseInt(Sseq);
+			System.out.println("삭제 시퀀스 : " + Sseq);
+
+			boolean isS = bbs.AfterdeletBbs(seq);
+			if (isS) {
+				req.setAttribute("msg2", "삭제했습니다");
+				dispatch("AfterBbsController?command=AfterBbslist", req, resp);
+			} else {
+				req.setAttribute("msg2", "삭제 실패");
+				dispatch("AfterBbsController?command=AfterBbsDetail&seq=" + seq, req, resp);
+			}
+
+		} else if (command.equals("AfterBbsSearch")) {
+
+			String Searchtype = req.getParameter("Searchtype"); // 검색종류(글쓴이,제목,내용)
+			String SearchWord = req.getParameter("SearchWord");
+
+			System.out.println(" search " + Searchtype + " word " + SearchWord);
+
+			List<AfterBbsDto> afterBbslist = bbs.getFindAfterlist(Searchtype, SearchWord);
+			// 짐 싸기
+			req.setAttribute("afterBbslist", afterBbslist);
+
+			// 보내주기
+			dispatch("AfterBbslist.jsp", req, resp);
+
+		}
+
 	}
-	
-	
-	
-	
+
 	public boolean isNull(String str) {
 		return str == null || str.trim().equals("");
 	}
-	
-	
-	
-	public void dispatch(String urls, HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+
+	public void dispatch(String urls, HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		RequestDispatcher dispatch = req.getRequestDispatcher(urls);
 		dispatch.forward(req, resp);
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
